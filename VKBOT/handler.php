ef3caf1c8b304f9f1e9aa362924efb1c134a0f68
zhel1 <30962820+zhel1@users.noreply.github.com>
2017-08-13 22:58:02 +0300

    123

--- VKBOT/handler.php
+++ VKBOT/handler.php
@@ -1,54 +1,90 @@
-<?php 
+<?php
 
-if (!isset($_REQUEST)) { 
-  return; 
-} 
-
+if (!isset($_REQUEST)) {
+    return;
+}
 
 //Строка для подтверждения адреса сервера из настроек Callback API 
 $confirmation_token = '4ac6eb71'; 
 
 //Ключ доступа сообщества 
 $token = 'fa951c328f76c1d0f6076f34284b428a8a64b84438758c4cbaa9c17f46250905632a0abaf0bd60adc4b92'; 
+// Secret key
 
-//Получаем и декодируем уведомление 
-$data = json_decode(file_get_contents('php://input')); 
+$secretKey = '551373455bobr';
 
-//Проверяем, что находится в поле "type" 
-switch ($data->type) { 
-  //Если это уведомление для подтверждения адреса... 
-  case 'confirmation': 
-    //...отправляем строку для подтверждения 
-    echo $confirmation_token; 
-    break; 
+//Получаем и декодируем уведомление
+$data = json_decode(file_get_contents('php://input'));
 
-//Если это уведомление о новом сообщении... 
-  case 'message_new': 
-    //...получаем id его автора 
-    $user_id = $data->object->user_id; 
-    //затем с помощью users.get получаем данные об авторе 
-    $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.0")); 
+// проверяем secretKey
+if (strcmp($data->secret, $secretKey) !== 0 && strcmp($data->type, 'confirmation') !== 0)
+    return;
 
-//и извлекаем из ответа его имя 
-    $user_name = $user_info->response[0]->first_name; 
+//Проверяем, что находится в поле "type"
+switch ($data->type) {
+    //Если это уведомление для подтверждения адреса сервера...
+    case 'confirmation':
+        //...отправляем строку для подтверждения адреса
+        echo $confirmationToken;
+        break;
 
-//С помощью messages.send отправляем ответное сообщение 
-    $request_params = array( 
-      'message' => "Hello, {$user_name}!", 
-      'user_id' => $user_id, 
-      'access_token' => $token, 
-      'v' => '5.0' 
-    ); 
+    //Если это уведомление о новом сообщении...
+    case 'message_new':
+        //...получаем id его автора
+        $userId = $data->object->user_id;
+        //затем с помощью users.get получаем данные об авторе
+        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&v=5.0"));
 
-$get_params = http_build_query($request_params); 
+        //и извлекаем из ответа его имя
+        $user_name = $userInfo->response[0]->first_name;
 
-file_get_contents('https://api.vk.com/method/messages.send?'. $get_params); 
+        //С помощью messages.send и токена сообщества отправляем ответное сообщение
+        $request_params = array(
+            'message' => "{$user_name}, ваше сообщение зарегистрировано!<br>" .
+                "Мы постараемся ответить в ближайшее время.",
+            'user_id' => $userId,
+            'access_token' => $token,
+            'v' => '5.0'
+        );
 
-//Возвращаем "ok" серверу Callback API 
+        $get_params = http_build_query($request_params);
 
-echo('ok'); 
+        file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
 
-break; 
+        //Возвращаем "ok" серверу Callback API
+        echo('ok');
 
-} 
-?> 
+        break;
+
+    // Если это уведомление о вступлении в группу
+    case 'group_join':
+        //...получаем id нового участника
+        $userId = $data->object->user_id;
+
+        //затем с помощью users.get получаем данные об авторе
+        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&v=5.0"));
+
+        //и извлекаем из ответа его имя
+        $user_name = $userInfo->response[0]->first_name;
+
+        //С помощью messages.send и токена сообщества отправляем ответное сообщение
+        $request_params = array(
+            'message' => "Добро пожаловать в наше сообщество МГТУ им. Баумана ИУ5 2016, {$user_name}!<br>" .
+                "Если у Вас возникнут вопросы, то вы всегда можете обратиться к администраторам сообщества.<br>" .
+                "Их контакты можно найти в соответсвующем разделе группы.<br>" .
+                "Успехов в учёбе!",
+            'user_id' => $userId,
+            'access_token' => $token,
+            'v' => '5.0'
+        );
+
+        $get_params = http_build_query($request_params);
+
+        file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
+
+        //Возвращаем "ok" серверу Callback API
+        echo('ok');
+
+        break;
+}
+?>
